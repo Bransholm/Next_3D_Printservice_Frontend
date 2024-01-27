3b7d1d968b45d8101be9ff42979d36b61c68a0df
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 20:27:34 +0100

    redundant code removed

--- admin/admin-view/admin-view-render-classes/catalogue-class.js
+++ admin/admin-view/admin-view-render-classes/catalogue-class.js
@@ -1,6 +1,4 @@
-// Eksempel på en klasse.
 export class catalogueItem {
-  // Klassens constructor tager vores fetchede-data-objekt som argument og sætter Klassens Atrributter lig Objektets properties.
   constructor(catalogueObject) {
     this.id = catalogueObject.Id;
     this.title = catalogueObject.Title;
@@ -11,25 +9,7 @@ export class catalogueItem {
     this.category = catalogueObject.Category;
   }
 
-  // I klassens render-metode bliver HTML'en til vores DOM lavet.
-  // Catalog DOM
-  render() {
-    const catalogueHTML =
-      /*html*/
-      `
-    <article>
-    <img src="../images/${this.imageLink}" alt="Produktbillede ${this.title}"/>
-    <h3>Produkt Navn: ${this.title}</h3>
-    <p>Kategori: ${this.category}</p>
-    <p>Standard Størrelse: ${this.standardSize} cm</p>
-    <p>Standard Vægt: ${this.standardWeight} gram</p>
-    <p>Produkt Beskrivelse: ${this.itemDescription} </p>
-    <button class="btn-view-product" >Se Produkt</button>
-    </article>
-    `;
-    return catalogueHTML;
-  }
-
+ // contains the DOM for the admin site  
   adminRender() {
     const adminCatalogueHTML =
       /*html*/
@@ -48,6 +28,4 @@ export class catalogueItem {
     `;
     return adminCatalogueHTML;
   }
-
-  // Vi skal have en metode til at kunne ændre i samtlige Attirbutter og opdatere databasen der efter - Lukas.
 }
