50e79c7b62310b6e3588b463d2ff2f3656828db6
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 17:27:49 +0100

    refactoring and dev comments

--- admin/admin-controller/finance-site.js
+++ admin/admin-controller/finance-site.js
@@ -4,19 +4,22 @@ import { showFinanceOveriew } from "../admin-view/finance-overview.js";
 let startDateValue;
 let endDateValue;
 
-async function submitFianceDates(event) {
+function submitFianceDates(event) {
   event.preventDefault();
-
+  // gets the values from the finance-form
   const form = document.querySelector("#finance-form");
-
   startDateValue = form.startDate.value;
   endDateValue = form.endDate.value;
+  // gets the finance data and passes it to the show-finance function
+  retrieveFinanceData();
+}
 
-  console.log("start", startDateValue);
-  console.log("end", endDateValue);
-
+async function retrieveFinanceData() {
+  // fetches the finance data from the
   const financeData = await getfinanceData();
+  // get the income amount from the finance data
   const incomeAmount = financeData[0].CombinedPrice;
+  // checks if the income is null or not
   if (incomeAmount != null) {
     showFinanceOveriew(startDateValue, endDateValue, incomeAmount);
   } else {
