d686288ea703727d113e11b2621b90082f3bffa5
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 22:39:44 +0100

    Update system-variables-put.js

--- admin/admin-model/backend-routes/system-variables-put.js
+++ admin/admin-model/backend-routes/system-variables-put.js
@@ -1,28 +1,28 @@
-// async function putExistingCustomer(data) {
-//   try {
-//     const response = await fetch(
-//       `${endpoint}..../${data.CustomerInfo.id}`,
-//       {
-//         method: "PUT",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(data.CustomerInfo),
-//       }
-//     );
+async function putSystemVariables(data) {
+  //   try {
+  //     const response = await fetch(
+  //       `${endpoint}..../${data.CustomerInfo.id}`,
+  //       {
+  //         method: "PUT",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(data.CustomerInfo),
+  //       }
+  //     );
+  //     console.log(response);
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! Status: ${response.status}`);
+  //     } else {
+  //       const result = await response.json();
+  //       console.log(result);
+  //     }
+  //     return response;
+  //   } catch (error) {
+  //     // Handle errors here
+  //     console.error("Error:", error);
+  //     throw error;
+  //   }
+}
 
-//     console.log(response);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     } else {
-//       const result = await response.json();
-//       console.log(result);
-//     }
-
-//     return response;
-//   } catch (error) {
-//     // Handle errors here
-//     console.error("Error:", error);
-//     throw error;
-//   }
-// }
+export { putSystemVariables };
