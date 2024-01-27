5ea36bd3d214cbed3f468ed93c829469fcd56a5f
Bransholm <kenneth@bransholm.dk>
2024-01-27 20:30:08 +0100

    Changeing customersObject.TimeDate format

--- admin/admin-view/admin-view-render-classes/customers-class.js
+++ admin/admin-view/admin-view-render-classes/customers-class.js
@@ -1,19 +1,36 @@
+//Creating class for customers
 class Customers {
-    constructor(customersObject) {
-        this.id = customersObject.Id;
-        this.firstName = customersObject.FirstName;
-        this.lastName = customersObject.LastName;
-        this.adress = customersObject.Adress;
-        this.zipCode = customersObject.ZipCode;
-        this.city = customersObject.City;
-        this.email = customersObject.Email;
-        this.timeDate = customersObject.TimeDate;
-    }
-    
-    render() {
-        const customerHTML =
-          /*html*/
-          `
+  constructor(customersObject) {
+    this.id = customersObject.Id;
+    this.firstName = customersObject.FirstName;
+    this.lastName = customersObject.LastName;
+    this.adress = customersObject.Adress;
+    this.zipCode = customersObject.ZipCode;
+    this.city = customersObject.City;
+    this.email = customersObject.Email;
+    this.timeDate = this.formatDateTime(customersObject.TimeDate);
+  }
+
+  // Changeing customersObject.TimeDate format
+  formatDateTime(dateTimeString) {
+    const originalDateTime = new Date(dateTimeString);
+    const formattedDate = originalDateTime.toLocaleDateString("da-DK", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    const formattedTime = originalDateTime.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return `${formattedDate} kl. ${formattedTime}`;
+  }
+
+// The dom manipulatuion
+  render() {
+    const customerHTML =
+      /*html*/
+      `
             <article>
                 <div id="customers_customer_id_headLine">Kunde id:${this.id}</div>
                 <div>Fornavn: ${this.firstName}</div>
@@ -26,8 +43,35 @@ class Customers {
             </article>
         
             `;
-        return customerHTML;
-    }
+    return customerHTML;
+  }
 }
+
+
+const yourObject = {
+  TimeDate: "2024-01-03T11:40:55.000Z",
+};
+
+const originalDateTime = new Date(yourObject.TimeDate);
+
+const formattedDate = originalDateTime.toLocaleDateString("da-DK");
+const formattedTime = originalDateTime.toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+const formattedDateTime = `${formattedDate} ${formattedTime}`;
+
+console.log(formattedDateTime);
+
+
+
+
+
+
+
+
+
+
 
 export { Customers }
