94d55502e132c44d6bc326f6813deeaefc42842d
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 22:40:40 +0100

    Update update-settings-variables.js

--- admin/admin-controller/update-settings-variables.js
+++ admin/admin-controller/update-settings-variables.js
@@ -1,10 +1,13 @@
 import { retrieveUpdateSettingsInput } from "./create-update-forms/update-system-variables.js";
+import { putSystemVariables } from "../admin-model/backend-routes/system-variables-put.js";
 
 async function submitUpdatedSettings() {
   const updateFormData = retrieveUpdateSettingsInput();
   updateSettingsVariables(updateFormData);
 }
 
-updateSettingsVariables(updateFormData);
+function updateSettingsVariables(updateFormData) {
+  putSystemVariables(updateFormData);
+}
 
 export { submitUpdatedSettings };
