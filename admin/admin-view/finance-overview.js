3dacef942a1f39b58c8f9f5e1f38df40c68efbeb
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 20:27:52 +0100

    comment changed

--- admin/admin-view/finance-overview.js
+++ admin/admin-view/finance-overview.js
@@ -1,6 +1,6 @@
 import { formateDate } from "./admin-view-helper-functions/formate-date.js";
 
-// re-sets the finance overivew
+// shows finance overivew
 function showFinanceOveriew(startDate, endDate, income) {
   document.querySelector("#finance-overview-container").innerHTML = `
   Indtægten for perioden ${formateDate(startDate)} til ${formateDate(
