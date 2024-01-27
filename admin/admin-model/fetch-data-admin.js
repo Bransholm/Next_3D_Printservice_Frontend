33eddf67a7b6d9de13dda556f6058663b530211c
Bransholm <kenneth@bransholm.dk>
2024-01-27 15:22:40 +0100

    getfinanceData fetch parth changed

--- admin/admin-model/fetch-data-admin.js
+++ admin/admin-model/fetch-data-admin.js
@@ -30,7 +30,7 @@ async function getAvailableStockData() {
 
 async function getfinanceData() {
   const response = await fetch(
-    `http://127.0.0.1:4811/finance?startDate=${startDateValue}&endDate=${endDateValue}`
+    `${endpoint}/finance?startDate=${startDateValue}&endDate=${endDateValue}`
   );
   console.log(
     `${endpoint}/finance?startDate=${startDateValue}&endDate=${endDateValue}`
