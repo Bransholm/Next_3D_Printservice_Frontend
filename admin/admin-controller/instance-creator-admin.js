de3820f352112b9b886be0344271748191f43ea7
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 18:22:48 +0100

    updated comments

--- admin/admin-controller/instance-creator-admin.js
+++ admin/admin-controller/instance-creator-admin.js
@@ -1,24 +1,17 @@
-// this creates an instancee of products
 
-/*
-HUSK FRONT ENDEN!!!
-  Vi kalder det i showCatalouge - admin main
-  Vi kalder det i show-stock-materials -
-*/
 
 // this function create classes for catalogue items
 export function createClassInstances(dataList, classType) {
   const instanceList = [];
 
-  // Loop to fetch the data
+  // loops every object in the data fetched from the database
   for (const object of dataList) {
-    // Hvert objekt i listen bliver nu lavet til en ny instans.
+    // a new instance of the class is now made with the object as its argument
     const newInstance = new classType(object);
 
     // the instance is pushed to our new list
     instanceList.push(newInstance);
   }
-  console.log("classlist: ", instanceList);
 
   // return the instance list
   return instanceList;
