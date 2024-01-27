3848bd296381abcb6816c39cc48ac7ccb9551428
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-01-27 20:27:19 +0100

    comments added

--- admin/admin-view/admin-view-helper-functions/formate-date.js
+++ admin/admin-view/admin-view-helper-functions/formate-date.js
@@ -1,5 +1,7 @@
+// alteres the date formate - not time
 function formateDate(date) {
   const newDate = new Date(date);
+  // sets the date formate to danish
   const formatedNewDate = new Intl.DateTimeFormat("dk", {
     dateStyle: "medium",
   }).format(newDate);
